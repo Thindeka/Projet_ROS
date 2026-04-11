301dd49a462ebc38c2f16f5a078e1fb62fbf09fd
Thindeka Mocumbi Tomas <thindeka@Thindekas-MacBook-Air-2.local>
2026-04-11 11:35:51 +0200

    Robot interagit avec le corridor mais probleme dans la logique

--- projet_ros2/projet_ros2/corridor_node.py
+++ projet_ros2/projet_ros2/corridor_node.py
@@ -23,10 +23,10 @@ class CorridorNode (Node) :
 
 
         # Paramètres
-        self.declare_parameters('linear_scale', 0.1)  
-        self.declare_parameters('angular_scale', 1.2)
-        self.declare_parameters('seuil_arret', 0.3)
-        self.declare_parameters('angular_scale_obstacle', 0.5)
+        self.declare_parameter('linear_scale', 0.1)  
+        self.declare_parameter('angular_scale', 1.2)
+        self.declare_parameter('seuil_arret', 0.15)
+        self.declare_parameter('angular_scale_obstacle', 0.5)
 
         self.linear_scale = float(self.get_parameter('linear_scale').value)      
         self.angular_scale = float(self.get_parameter('angular_scale').value) 
@@ -35,12 +35,14 @@ class CorridorNode (Node) :
 
         # Secteurs angulaires en dégrés : on prend des secteurs au lieu d'une seule mesure pour avoir des mesures plus stables
         self.declare_parameter('left_center_deg', 90.0)
-        self.declare_parameter('right_center_deg', -90.0)
-        self.declare_parameter('side_window_deg', 20.0)
-        self.declare_parameter('front_window_deg', 15.0)
+        self.declare_parameter('right_center_deg', 270.0)
+        self.declare_parameter('front_center_deg', 180.0)
+        self.declare_parameter('side_window_deg', 30.0)
+        self.declare_parameter('front_window_deg', 20.0)
 
         self.left_center_deg = float(self.get_parameter('left_center_deg').value)
         self.right_center_deg = float(self.get_parameter('right_center_deg').value)
+        self.front_center_deg = float(self.get_parameter('front_center_deg').value)
         self.side_window_deg = float(self.get_parameter('side_window_deg').value)
         self.front_window_deg = float(self.get_parameter('front_window_deg').value)
 
@@ -61,43 +63,75 @@ class CorridorNode (Node) :
 
 
 
-    def scan_callback (self, msg) :
+    def scan_callback (self, msg : LaserScan) :
 
+        """
         dist_gauche = self.moyenne_secteur(
             msg,
             self.left_center_deg - self.side_window_deg,
             self.left_center_deg + self.side_window_deg
         )
 
-        dist_droite = self.get_sector_mean(
+        dist_droite = self.moyenne_secteur(
             msg,
             self.right_center_deg - self.side_window_deg,
             self.right_center_deg + self.side_window_deg
         )
 
-        diste_avant = self.get_sector_min(
+        dist_avant = self.min_secteur(
             msg,
-            -self.front_window_deg,
+            360.0 - self.front_window_deg,
             self.front_window_deg
         )
+        """
+        
+
+        # DEBUG 
+        valeurs_gauche = self.get_valeurs(
+            msg,
+            self.left_center_deg - self.side_window_deg,
+            self.left_center_deg + self.side_window_deg
+        )
+
+        valeurs_droite = self.get_valeurs(
+            msg,
+            self.right_center_deg - self.side_window_deg,
+            self.right_center_deg + self.side_window_deg
+        )
+
+        valeurs_avant = self.get_valeurs(
+            msg,
+            self.front_center_deg - self.front_window_deg,
+            self.front_center_deg + self.front_window_deg
+        )
+
+        dist_gauche = sum(valeurs_gauche) / len(valeurs_gauche) if valeurs_gauche else None
+        dist_droite = sum(valeurs_droite) / len(valeurs_droite) if valeurs_droite else None
+        dist_avant = min(valeurs_avant) if valeurs_avant else None
 
         cmd = Twist()
 
 
         # Fallback 
-        if dist_gauche is None or dist_droite is None or diste_avant is None :
+        if dist_gauche is None or dist_droite is None or dist_avant is None :
             cmd.linear.x = 0.0  # on n'avance pas
             cmd.angular.z = 0.0 # on ne tourne pas
             self.cmd_pub.publish(cmd)
-            self.get_logger().warn('Data du /scan invalide, arrêt du robot')
+            self.get_logger().warn(
+                f'/scan invalide | nb_gauche={len(valeurs_gauche)} '
+                f'nb_droite={len(valeurs_droite)} nb_avant={len(valeurs_avant)}'
+            )
+            return
 
 
         # Sécurité
-        if diste_avant < self.seuil_arret :
+        if dist_avant < self.seuil_arret and abs(dist_gauche - dist_droite) < 0.2 :
+            # si mur devant ET robot centré => obstacle réel => tourner
+            # mais si mur devant + déséquilibre gauche/droite => c'est un virage => continuer à avancer
             
-            cmd.linear.x = 0.0 # on n'avance pas
+            cmd.linear.x = 0.5 # robor sort du blocage très lentement
 
-            # on s'éloigne en tournant du côté le plus proche 
+            # on tourne du côté où il y a le plus d'espace
             # A VOIR SI LE SENS EST BON
             if dist_gauche > dist_droite :
                 cmd.angular.z = self.angular_scale_obstacle
@@ -115,12 +149,12 @@ class CorridorNode (Node) :
 
 
         now = self.get_clock().now()
-        now = self.get_clock().now()
+
         if (now - self.last_log_time).nanoseconds > 5e8:  # 0.5 s
             error = dist_gauche - dist_droite
             self.get_logger().info(
                 f'gauche={dist_gauche:.3f} droite={dist_droite:.3f} '
-                f'avant={diste_avant:.3f} erreur={error:.3f}'
+                f'avant={dist_avant:.3f} erreur={error:.3f}'
             )
             self.last_log_time = now
 
@@ -146,34 +180,33 @@ class CorridorNode (Node) :
             return None
         return min(valeurs)
 
-
-
-    def get_valeurs (self, scan, deg_debut, deg_fin) :
-        """
-        Retourne toutes les distances du LiDAR dans une certaine direction (angle)
-        ex : toutes les distances entre 70° et 110° (mur gauche)
-        """
-        
-        # conversion en radians
-        rad_debut = math.radians(deg_debut)
-        rad_fin = math.radians(deg_fin)
-        
-        if rad_debut > rad_fin :
-            rad_debut, rad_fin = rad_fin, rad_debut
+    def get_valeurs(self, scan, deg_debut, deg_fin):
+        rad_debut = math.radians(deg_debut) % (2.0 * math.pi)
+        rad_fin = math.radians(deg_fin) % (2.0 * math.pi)
 
         valeurs = []
 
-        # on boucle sur toutes les mesures
-        for i,r in enumerate(scan.ranges) :
-            
-            angle = scan.angle_min + i * scan.anngle_increment
-            
-            if rad_debut <= angle <= rad_fin : # on filtre la zone
-                if math.isfinite(r) and scan.range_min < r < scan.range_max :
+        for i, r in enumerate(scan.ranges):
+            angle = (scan.angle_min + i * scan.angle_increment) % (2.0 * math.pi)
+
+            # secteur normal
+            if rad_debut <= rad_fin:
+                dans_secteur = rad_debut <= angle <= rad_fin
+            # secteur qui traverse 0°
+            else:
+                dans_secteur = angle >= rad_debut or angle <= rad_fin
+
+            if dans_secteur:
+                if math.isnan(r):
+                    continue
+                if math.isinf(r):
+                    valeurs.append(scan.range_max)
+                elif scan.range_min < r < scan.range_max:
                     valeurs.append(r)
 
         return valeurs
 
+    
 
 
 
@@ -183,7 +216,7 @@ def main(args=None) :
     node = CorridorNode()
 
     try :
-        rclpy.spin(Node)
+        rclpy.spin(node)
 
     except KeyboardInterrupt :
         pass
